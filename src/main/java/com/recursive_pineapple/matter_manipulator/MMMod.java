f77a136f5997925c7169e3596b86b51698cefef3
RecursivePineapple <recursive_pineapple@proton.me>
2024-12-20 16:17:06 -0500

    remove postea migration

--- src/main/java/com/recursive_pineapple/matter_manipulator/MMMod.java
+++ src/main/java/com/recursive_pineapple/matter_manipulator/MMMod.java
@@ -4,11 +4,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLMissingMappingsEvent;
+import cpw.mods.fml.common.event.FMLMissingMappingsEvent.MissingMapping;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.registry.GameRegistry.Type;
 
 @Mod(modid = MMMod.MODID, version = Tags.VERSION, name = "Matter Manipulator", acceptedMinecraftVersions = "[1.7.10]")
 public class MMMod {
@@ -39,5 +43,17 @@ public class MMMod {
     @Mod.EventHandler
     public void serverStarting(FMLServerStartingEvent event) {
         proxy.serverStarting(event);
+    }
+
+    @EventHandler
+    public void missingMapping(FMLMissingMappingsEvent event) {
+        for (MissingMapping mapping : event.getAll()) {
+            if (mapping.type != Type.ITEM) continue;
+            if (mapping.name.equals("gregtech:gt.metaitem.04")) mapping.ignore();
+            if (mapping.name.equals("gregtech:itemMatterManipulator0")) mapping.ignore();
+            if (mapping.name.equals("gregtech:itemMatterManipulator1")) mapping.ignore();
+            if (mapping.name.equals("gregtech:itemMatterManipulator2")) mapping.ignore();
+            if (mapping.name.equals("gregtech:itemMatterManipulator3")) mapping.ignore();
+        }
     }
 }
