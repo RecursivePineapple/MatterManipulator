2e70b6490f939173d1a920ab162d7584f4243578
RecursivePineapple <recursive_pineapple@proton.me>
2024-12-28 15:14:16 -0500

    fix item deletion bug

--- src/main/java/com/recursive_pineapple/matter_manipulator/common/building/PendingBuild.java
+++ src/main/java/com/recursive_pineapple/matter_manipulator/common/building/PendingBuild.java
@@ -283,7 +283,7 @@ public class PendingBuild extends AbstractBuildable {
                     visited.remove(coord);
                 }
                 
-                return;
+                toPlace.clear();
             }
         }
 
